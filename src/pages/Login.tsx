82be10e95fc24d485c3f4ee61dd91d031225c08b
Kenny Nguyen <sokenns@gmail.com>
2024-07-24 15:22:33 -0700

    Add simple form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,19 @@
 function Login() {
-  return <div>Login Page</div>;
+  return (
+    <div>
+      <form action="" className="test-border">
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
