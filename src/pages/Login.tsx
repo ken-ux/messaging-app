eed61377c2ff10319b98822aa7ffdfa8b7754829
Kenny Nguyen <sokenns@gmail.com>
2024-07-25 01:23:49 -0700

    Add link to register

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,36 @@
 function Login() {
   return (
-    <div>
-      <form action="" className="test-border">
+    <main>
+      <form
+        action=""
+        className="test-border mx-auto flex max-w-xl flex-col items-center"
+      >
         <div>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            autoComplete="username"
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            autoComplete="current-password"
+          />
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" className="bg-slate-500">
+          Login
+        </button>
+        <p>
+          Don't have an account? <a href="/">Register</a>.
+        </p>
       </form>
-    </div>
+    </main>
   );
 }
 
