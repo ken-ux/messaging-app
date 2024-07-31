457ddff27d82c3571dcff244fd923db6a821efbc
Kenny Nguyen <sokenns@gmail.com>
2024-07-31 14:46:20 -0700

    Create wrapper for main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Outlet } from "react-router-dom";
+import ContentWrapper from "./components/ContentWrapper";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="flex">
       <Nav />
-      <main>
-        <p className="bg-blue-200 text-2xl">Main</p>
+      <main className="mx-auto">
+        <ContentWrapper>
+          <p className="bg-blue-200 text-2xl">Main Content Below</p>
+          <Outlet />
+        </ContentWrapper>
       </main>
     </div>
   );
---
+++ src/components/ContentWrapper.tsx
@@ -0,0 +1,5 @@
+function ContentWrapper({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
+
+export default ContentWrapper;
