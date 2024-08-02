19b8498ffc60d5aaf27995b9bb30a082bb99f2d2
Kenny Nguyen <sokenns@gmail.com>
2024-08-02 13:27:32 -0700

    Style wrapper with colors to plan layout

--- src/components/ContentWrapper.tsx
+++ src/components/ContentWrapper.tsx
@@ -2,11 +2,11 @@ import { XMarkIcon } from "@heroicons/react/20/solid";
 
 function ContentWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <div className="test-border min-w-96">
-      <div className="test-border flex h-6 justify-end">
+    <div className="test-border min-w-96 bg-green-100">
+      <div className="m-2 flex h-6 justify-end bg-red-100">
         <XMarkIcon className="h-full" />
       </div>
-      {children}
+      <div className="m-2 bg-red-100 p-2">{children}</div>
     </div>
   );
 }
