b673b73a5e8d140da8879d31a5d4e9795e8fa9e8
Kenny Nguyen <sokenns@gmail.com>
2024-08-05 02:39:39 -0700

    Add form to settings page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,5 +1,19 @@
 function SettingsPage() {
-  return <div>Settings</div>;
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="display_name">Display Name</label>
+        <input type="text" id="display_name" name="display_name" />
+      </div>
+      <div>
+        <label htmlFor="color">Profile Color</label>
+        <input type="color" id="color" name="color" />
+      </div>
+      <button type="submit" className="bg-slate-100">
+        Save
+      </button>
+    </form>
+  );
 }
 
 export default SettingsPage;
