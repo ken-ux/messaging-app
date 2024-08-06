8f2482acac9b1857f63c971cecdd314c123f81e2
Kenny Nguyen <sokenns@gmail.com>
2024-08-06 11:59:45 -0700

    Add input field for user description

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 function SettingsPage() {
+  const [textLength, setTextLength] = useState(0);
+
   return (
     <form action="">
       <div>
         <label htmlFor="display_name">Display Name</label>
-        <input type="text" id="display_name" name="display_name" />
+        <input type="text" id="display_name" name="display_name" maxLength={20} />
+      </div>
+      <div>
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          maxLength={100}
+          onChange={(e) => {
+            setTextLength(e.target.value.length);
+          }}
+        />
+        <span>{textLength}/100</span>
       </div>
       <div>
         <label htmlFor="color">Profile Color</label>
