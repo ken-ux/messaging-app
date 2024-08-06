1b04e8d102075f268ebb2c0d0d23b9260547e159
Kenny Nguyen <sokenns@gmail.com>
2024-08-06 14:01:30 -0700

    Add placeholder content and form to message page

--- src/pages/MessagePage.tsx
+++ src/pages/MessagePage.tsx
@@ -1,5 +1,28 @@
+import { useParams } from "react-router-dom";
+
 function MessagePage() {
-  return <div>Message</div>;
+  const { user } = useParams();
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="max-h-96 bg-white p-2">
+        <p>{user}: Hello</p>
+        <p>me: hi</p>
+      </div>
+
+      <form action="" className="flex gap-2">
+        <textarea
+          name="message"
+          id="message"
+          aria-label="message"
+          className="grow"
+        ></textarea>
+        <button type="submit" className="bg-slate-100 px-4">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default MessagePage;
