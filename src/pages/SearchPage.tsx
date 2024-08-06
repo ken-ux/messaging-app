07f38d937fbe72716aec50da36f809ee07f1d0fc
Kenny Nguyen <sokenns@gmail.com>
2024-08-06 13:20:34 -0700

    Add form to search page

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+
 function SearchPage() {
-  return <div>Search</div>;
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="user">Search for User</label>
+          <input type="text" id="user" name="user" />
+        </div>
+        <button type="submit" className="bg-slate-100">
+          Search
+        </button>
+      </form>
+      <p className="mt-2">Search Results</p>
+      <ul>
+        <li>
+          <Link to="/message/fake">Fake User</Link>
+        </li>
+        <li>
+          <Link to="/message/fake">Fake User</Link>
+        </li>
+        <li>
+          <Link to="/message/fake">Fake User</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SearchPage;
