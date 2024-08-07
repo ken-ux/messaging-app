77fb2767399a1c6e4cb5a7cc415c87727725891f
Kenny Nguyen <sokenns@gmail.com>
2024-08-07 14:16:40 -0700

    Add server config for running in Docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Uncomment the next lines to run in Docker container.
+  // server: {
+  //   host: true,
+  //   port: 5173
+  // },
+});
