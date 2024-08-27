741c2c3e8386997504ba02c36240758ce40eabd2
Kenny Nguyen <sokenns@gmail.com>
2024-08-26 21:36:50 -0700

    Redirect user to login page if not authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import ContentWrapper from "./components/ContentWrapper";
 import Nav from "./components/Nav";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+
+    // Check for token.
+    if (!token) {
+      navigate("/login");
+    }
+
+    // Check if token is valid.
+    const auth = async () => {
+      const url = import.meta.env.VITE_API_URL + "/auth";
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+        body: JSON.stringify({ username: localStorage.getItem("user") }),
+      });
+
+      const valid = await response.json();
+      if (!valid) {
+        navigate("/login");
+      }
+    };
+
+    if (token) {
+      auth();
+    }
+
+    return () => {
+      token = null;
+    };
+  }, [navigate]);
+
   return (
     <div className="flex">
       <Nav />
