53f381c65f3609955fd1ad539aa274f510505ec2
Kenny Nguyen <sokenns@gmail.com>
2024-09-04 16:41:44 -0700

    Trim whitespace from input and run queries onChange instead of onSubmit

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -11,20 +11,25 @@ function SearchPage() {
     const element = e.currentTarget.elements as HTMLFormControlsCollection & {
       username: HTMLInputElement;
     };
-    const searchQuery = element["username"].value;
-    try {
-      const url = import.meta.env.VITE_API_URL + "/search?username=";
-      const response = await fetch(url + searchQuery);
-      const data = await response.json();
-      setSearchResults(data);
-    } catch (err) {
-      console.log(err);
+    let searchQuery = element["username"].value;
+    searchQuery = searchQuery.trim();
+    if (searchQuery !== "") {
+      try {
+        const url = import.meta.env.VITE_API_URL + "/search?username=";
+        const response = await fetch(url + searchQuery);
+        const data = await response.json();
+        setSearchResults(data);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      setSearchResults(null);
     }
   };
 
   return (
     <div>
-      <form onSubmit={(e) => formHandler(e)}>
+      <form onChange={(e) => formHandler(e)}>
         <div>
           <label htmlFor="username">Search for User</label>
           <input
@@ -34,9 +39,6 @@ function SearchPage() {
             pattern="[a-zA-Z0-9]+"
           />
         </div>
-        <button type="submit" className="bg-slate-100">
-          Search
-        </button>
       </form>
       <p className="mt-2">Search Results</p>
       <ul>
