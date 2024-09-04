35b0d99f0aa9c97a98f92b08e045a7412fbef74d
Kenny Nguyen <sokenns@gmail.com>
2024-09-04 14:31:41 -0700

    Add form handler for searching users

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function SearchPage() {
+  const [searchResults, setSearchResults] = useState<
+    { username: string }[] | null
+  >(null);
+
+  const formHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const element = e.currentTarget.elements as HTMLFormControlsCollection & {
+      username: HTMLInputElement;
+    };
+    const searchQuery = element["username"].value;
+    try {
+      const url = import.meta.env.VITE_API_URL + "/search?username=";
+      const response = await fetch(url + searchQuery);
+      const data = await response.json();
+      setSearchResults(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={(e) => formHandler(e)}>
         <div>
-          <label htmlFor="user">Search for User</label>
-          <input type="text" id="user" name="user" />
+          <label htmlFor="username">Search for User</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            pattern="[a-zA-Z0-9]+"
+          />
         </div>
         <button type="submit" className="bg-slate-100">
           Search
@@ -14,15 +40,15 @@ function SearchPage() {
       </form>
       <p className="mt-2">Search Results</p>
       <ul>
-        <li>
-          <Link to="/message/fake">Fake User</Link>
-        </li>
-        <li>
-          <Link to="/message/fake">Fake User</Link>
-        </li>
-        <li>
-          <Link to="/message/fake">Fake User</Link>
-        </li>
+        {searchResults !== null
+          ? searchResults.map((user) => {
+              return (
+                <li key={user.username}>
+                  <Link to={"/message/" + user.username}>{user.username}</Link>
+                </li>
+              );
+            })
+          : "No Results"}
       </ul>
     </div>
   );
