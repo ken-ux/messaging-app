f4314a3f0684e166cb9797369f88445bfe15f46f
Kenny Nguyen <sokenns@gmail.com>
2024-09-20 01:40:52 -0700

    Render queried messages

--- src/pages/MessagePage.tsx
+++ src/pages/MessagePage.tsx
@@ -90,15 +90,25 @@ function MessagePage() {
     }
   };
 
+  let messageList;
   if (messagesLoaded) {
-    console.log("messages are ready");
+    if (messages.current !== null) {
+      messageList = messages.current.map((message, id) => {
+        return (
+          <p key={id}>
+            {message.sender}: {message.message_body}
+          </p>
+        );
+      });
+    } else {
+      messageList = <p>No messages. :-(</p>;
+    }
   }
 
   return (
     <div className="flex flex-col gap-2">
       <div className="max-h-96 bg-white p-2">
-        <p>{user}: Hello</p>
-        <p>me: hi</p>
+        {messagesLoaded ? messageList : <p>Messages loading...</p>}
       </div>
 
       <form onSubmit={formHandler} className="flex gap-2">
