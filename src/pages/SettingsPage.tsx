5441b82bee9df2a61a6bd01f7207c90c214bc4cf
Kenny Nguyen <sokenns@gmail.com>
2024-09-24 14:26:39 -0700

    Add form handler for saving settings

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -2,9 +2,49 @@ import { useState } from "react";
 
 function SettingsPage() {
   const [textLength, setTextLength] = useState(0);
+  const [message, setMessage] = useState("");
+
+  const formHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const elements = e.currentTarget.elements as HTMLFormControlsCollection & {
+      description: HTMLTextAreaElement;
+      color: HTMLInputElement;
+    };
+
+    const formData = {
+      description: elements.description.value,
+      color: elements.color.value,
+    };
+
+    try {
+      const user = localStorage.getItem("user");
+      const url = import.meta.env.VITE_API_URL + "/profile?username=" + user;
+
+      const response = await fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.status === 200) {
+        setMessage("Settings saved!");
+        setTimeout(() => {
+          setMessage("");
+        }, 4000);
+      } else {
+        setMessage("Error saving settings. Try again later.");
+      }
+    } catch (err) {
+      setMessage("Error saving settings. Try again later.");
+    }
+  };
 
   return (
-    <form action="">
+    <form onSubmit={formHandler}>
       <div>
         <label htmlFor="description">Description</label>
         <textarea
@@ -24,6 +64,7 @@ function SettingsPage() {
       <button type="submit" className="bg-slate-100">
         Save
       </button>
+      <p>{message}</p>
     </form>
   );
 }
