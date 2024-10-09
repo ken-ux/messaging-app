b8b2ab958f0100f2ef530f92d722f92ed1c85cca
Kenny Nguyen <sokenns@gmail.com>
2024-10-09 12:15:39 -0700

    Style registration page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,15 +1,22 @@
+import {
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/20/solid";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../utils";
 
 function RegisterPage() {
   const navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = useState("");
+  const [message, setMessage] = useState("");
+  const [valid, setValid] = useState(false);
+  const [disabled, setDisabled] = useState(false);
   const [password, setPassword] = useState("");
   const [passwordsMatch, setPasswordsMatch] = useState(false);
 
   const formHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setDisabled(true);
 
     if (!passwordsMatch) {
       return;
@@ -39,7 +46,8 @@ function RegisterPage() {
       });
 
       if (response.status === 200) {
-        setErrorMessage("Success! Redirecting to the homepage.");
+        setValid(true);
+        setMessage("Success! Redirecting to the homepage.");
         const jwt = await response.json();
         localStorage.setItem("token", jwt);
         localStorage.setItem("user", formData.username);
@@ -48,12 +56,14 @@ function RegisterPage() {
         }, 1000);
       } else {
         const message = await response.text();
-        setErrorMessage(message);
+        setMessage(message);
+        setDisabled(false);
       }
     } catch (error) {
-      setErrorMessage(
+      setMessage(
         "Error processing your request, try again or contact site owner.",
       );
+      setDisabled(false);
     }
   };
 
@@ -74,65 +84,98 @@ function RegisterPage() {
   }, [navigate]);
 
   return (
-    <main>
+    <main className="m-5">
       <form
         onSubmit={(e) => formHandler(e)}
-        className="test-border mx-auto flex max-w-xl flex-col items-center"
+        className="mx-auto flex max-w-xl flex-col items-center gap-4 rounded bg-indigo-500 p-5 text-white"
       >
-        <div>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            autoComplete="username"
-            pattern="[a-zA-Z0-9]+"
-            minLength={5}
-            maxLength={20}
-            required
-          />
+        <h1 className="my-2 text-3xl">StarSend</h1>
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between gap-2">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              autoComplete="username"
+              pattern="[a-zA-Z0-9]+"
+              minLength={5}
+              maxLength={20}
+              required
+              className="text-input text-black"
+            />
+          </div>
+          <div className="flex justify-between gap-2">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              autoComplete="new-password"
+              minLength={5}
+              maxLength={20}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              required
+              className="text-input text-black"
+            />
+          </div>
+          <div className="flex justify-between gap-2">
+            <label htmlFor="confirm_password">Re-enter Password</label>
+            <input
+              type="password"
+              id="confirm_password"
+              autoComplete="new-password"
+              minLength={5}
+              maxLength={20}
+              required
+              onChange={(e) => {
+                if (e.target.value !== password) {
+                  setPasswordsMatch(false);
+                  setMessage("Passwords do not match.");
+                } else {
+                  setPasswordsMatch(true);
+                  setMessage("");
+                }
+              }}
+              className="text-input text-black"
+            />
+          </div>
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            autoComplete="new-password"
-            minLength={5}
-            maxLength={20}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="confirm_password">Confirm Password</label>
-          <input
-            type="password"
-            id="confirm_password"
-            autoComplete="new-password"
-            minLength={5}
-            maxLength={20}
-            required
-            onChange={(e) => {
-              if (e.target.value !== password) {
-                setPasswordsMatch(false);
-                setErrorMessage("Passwords do not match.");
-              } else {
-                setPasswordsMatch(true);
-                setErrorMessage("");
-              }
-            }}
-          />
-        </div>
-        <button type="submit" className="bg-slate-500">
+        <button
+          type="submit"
+          className={
+            "rounded bg-indigo-700 px-2 py-1 " +
+            (disabled && "bg-indigo-300 text-indigo-400")
+          }
+          disabled={disabled}
+        >
           Register
         </button>
-        <p>{errorMessage}</p>
+        {message && (
+          <div
+            className={
+              "flex items-center gap-2 rounded px-2 py-1 font-medium " +
+              (valid
+                ? "bg-green-100 text-green-700"
+                : "bg-red-100 text-red-700")
+            }
+          >
+            {valid ? (
+              <CheckCircleIcon className="h-5 w-5" />
+            ) : (
+              <ExclamationCircleIcon className="h-5 w-5" />
+            )}
+            <p>{message}</p>
+          </div>
+        )}
         <p>
-          Already have an account? <Link to="/login">Login</Link>.
+          Already have an account?{" "}
+          <Link to="/login" className="underline">
+            Login
+          </Link>
+          .
         </p>
       </form>
     </main>
