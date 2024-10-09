9ac977d4dd298dcda9707f527b2792aac9c455c3
Kenny Nguyen <sokenns@gmail.com>
2024-10-09 16:05:16 -0700

    Style error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 function ErrorPage() {
-  return <div>Error Page</div>;
+  return (
+    <main className="flex h-screen flex-col items-center justify-center gap-5">
+      <h1 className="rounded bg-indigo-100 px-4 py-2 text-2xl text-indigo-700">
+        Error 404: Page does not exist.
+      </h1>
+      <Link to="/" className="text-lg text-indigo-500 underline">
+        Go to home page.
+      </Link>
+    </main>
+  );
 }
 
 export default ErrorPage;
